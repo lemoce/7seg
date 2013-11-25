50117023350b25dc79c493707680d322161ea3fb
lemoce <cerencio@yahoo.com.br>
2013-11-25 02:27:56 -0200

    Correcao main.c

--- main.c
+++ main.c
@@ -4,24 +4,24 @@
 
 #include <inttypes.h>
 
-int main( void )
+int main (void)
 {
   SeteSeg * seg;
 
   char on = 0;
-  seg = seteSeg_create(6, 0, on);
+  seg = seteSeg_create (6, 0, on);
   int hex = 0x10;
-  while( 1 ) 
+  while (1) 
     {
-      for(int i = hex; i >= 0; i--)
+      for (int i = hex; i >= 0; i--)
 	{
 	  on = !on;
-	  seteSeg_set_dot(seg, on);
-	  seteSeg_print_hex(seg, i);
-	  _delay_ms(1000);
+	  seteSeg_set_dot (seg, on);
+	  seteSeg_print_hex (seg, i);
+	  _delay_ms (1000);
 	}
       seteSeg_clean (seg);
-      _delay_ms(1000);
+      _delay_ms (1000);
     }
   
   seteSeg_destroy( seg );
